68a576620c727f460649b8b9d04155e6bfd24886
Steve Dee <steve@smartercode.net>
2012-05-05 22:21:54 -0700

    Trailing whitespace

--- ex1/plotData.m
+++ ex1/plotData.m
@@ -1,5 +1,5 @@
 function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
+%PLOTDATA Plots the data points x and y into a new figure
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
 
