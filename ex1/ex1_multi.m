567b52b971d28d1593396757ab32034a50a9c205
Steve Dee <steve@smartercode.net>
2012-05-06 23:16:54 -0700

    Predict price

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -101,13 +101,7 @@ fprintf(' %f \n', theta);
 fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
-% ====================== YOUR CODE HERE ======================
-% Recall that the first column of X is all-ones. Thus, it does
-% not need to be normalized.
-price = 0; % You should change this
-
-
-% ============================================================
+price = [1, (1650 - mu(1)) / sigma(1),  (3 - mu(2)) / sigma(2)] * theta;
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
