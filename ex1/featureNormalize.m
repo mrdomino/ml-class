ba126c2769d74b3bccb76ee11b89da051b82919e
Steve Dee <steve@smartercode.net>
2012-05-06 23:37:11 -0700

    Use bsxfun for featureNormalize

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -7,10 +7,6 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 mu = mean(X);
 sigma = std(X);
-for i = 1:size(X, 1),
-    for j = 1:size(X, 2),
-        X_norm(i, j) = (X(i, j) - mu(j)) / sigma(j);
-    end
-end
+X_norm = bsxfun(@rdivide, bsxfun(@minus, X, mu), sigma);
 
 end
