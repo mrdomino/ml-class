707fdb3d5cd46d3c9d35546500857bac286b4a1d
Steve Dee <steve@smartercode.net>
2012-05-06 23:07:25 -0700

    Improved computeCostMulti from ex1.pdf

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -5,6 +5,6 @@ function J = computeCostMulti(X, y, theta)
 
 m = length(y); % number of training examples
 
-J = 1 / (2 * m) * sum((X * theta - y) .^ 2);
+J = 1 / (2 * m) * (X * theta - y)' * (X * theta - y);
 
 end
