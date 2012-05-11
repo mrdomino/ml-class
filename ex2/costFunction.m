3fb5c436589bb07e141f53644e2e3cb3d77f80ac
Steve Dee <steve@smartercode.net>
2012-05-11 14:56:48 -0700

    Clean up costFunction

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -8,26 +8,9 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 h = sigmoid(X * theta); % Vector of hypothesis values
 
-% You need to return the following variables correctly 
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
 J = 1 / m * (-y' * log(h) - (1 .- y') * log(1 .- h));
 
-
-
-
-
-% =============================================================
+% TODO: Implement grad.
+grad = zeros(size(theta));
 
 end
