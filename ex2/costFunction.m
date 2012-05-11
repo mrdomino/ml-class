ca66a20d16e20259820126d9e8381dbe0c013e50
Steve Dee <steve@smartercode.net>
2012-05-11 14:56:46 -0700

    Vectorize J

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -8,16 +8,9 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 grad = zeros(size(theta));
 
-h = @(x) sigmoid(theta' * x);
-
-for i = 1:m,
-  J = J + (-y(i) * log(h(X(i, :)')) - (1 - y(i)) * log(1 - h(X(i, :)')));
-end
-
-J = J / m;
+J = (-y' * log(sigmoid(X * theta)) - (1 .- y') * log(1 .- sigmoid(X * theta))) / m;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
