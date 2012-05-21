55a97431c79e9b35586c86c71532fbb561330752
Steve Dee <steve@smartercode.net>
2012-05-21 14:09:21 -0700

    Implement lrCostFunction
    Trivially from ex2's costFunction and costFunctionReg.

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -7,46 +7,13 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta);
+h = sigmoid(X * theta); % Vector of hypothesis values
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+J = (1 / m * (-y' * log(h) - (1 .- y') * log(1 .- h))
+     + lambda / (2 * m) * theta(2:n)' * theta(2:n));
+grad = 1 / m * X' * (h .- y);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Hint: The computation of the cost function and gradients can be
-%       efficiently vectorized. For example, consider the computation
-%
-%           sigmoid(X * theta)
-%
-%       Each row of the resulting matrix will contain the value of the
-%       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
-%
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
-grad = grad(:);
+grad(2:n) = grad(2:n) + theta(2:n) * lambda / m;
 
 end
