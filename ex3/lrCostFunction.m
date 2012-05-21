bb89b1548e7529c057457b362e26ff337a3da293
Steve Dee <steve@smartercode.net>
2012-05-21 14:13:42 -0700

    Use `end` keyword

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -7,13 +7,12 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-n = length(theta);
 h = sigmoid(X * theta); % Vector of hypothesis values
 
 J = (1 / m * (-y' * log(h) - (1 .- y') * log(1 .- h))
-     + lambda / (2 * m) * theta(2:n)' * theta(2:n));
+     + lambda / (2 * m) * theta(2:end)' * theta(2:end));
 grad = 1 / m * X' * (h .- y);
 
-grad(2:n) = grad(2:n) + theta(2:n) * lambda / m;
+grad(2:end) = grad(2:end) + theta(2:end) * lambda / m;
 
 end
