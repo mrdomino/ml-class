341b2998f998acc7385f006169dd01daf43507a6
Steve Dee <steve@smartercode.net>
2012-05-23 13:57:59 -0700

    Simplify m, n

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -8,8 +8,7 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   to the classifier for label i
 
 % Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+[m, n] = size(X);
 
 % You need to return the following variables correctly
 all_theta = zeros(num_labels, n + 1);
