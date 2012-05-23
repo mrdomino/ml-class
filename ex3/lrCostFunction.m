2f41024a0a3c36e5cdcdbb6fe957e3ad6aa2bf7c
Steve Dee <steve@smartercode.net>
2012-05-23 14:01:55 -0700

    Use ... for line continuation

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -9,8 +9,8 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 h = sigmoid(X * theta); % Vector of hypothesis values
 
-J = (1 / m * (-y' * log(h) - (1 .- y') * log(1 .- h))
-     + lambda / (2 * m) * theta(2:end)' * theta(2:end));
+J = 1 / m * (-y' * log(h) - (1 .- y') * log(1 .- h)) ...
+  + lambda / (2 * m) * theta(2:end)' * theta(2:end);
 
 grad = 1 / m * X' * (h .- y);
 
