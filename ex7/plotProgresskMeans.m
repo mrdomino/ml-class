e3dbc7d1b6b9e97afb3534f4771b4072ee890ccb
Steve Dee <steve@smartercode.net>
2012-06-26 20:40:38 -0700

    Remove trailing whitespace

--- ex7/plotProgresskMeans.m
+++ ex7/plotProgresskMeans.m
@@ -1,5 +1,5 @@
 function plotProgresskMeans(X, centroids, previous, idx, K, i)
-%PLOTPROGRESSKMEANS is a helper function that displays the progress of 
+%PLOTPROGRESSKMEANS is a helper function that displays the progress of
 %k-Means as it is running. It is intended for use only with 2D data.
 %   PLOTPROGRESSKMEANS(X, centroids, previous, idx, K, i) plots the data
 %   points with colors assigned to each centroid. With the previous
--- ex7/runkMeans.m
+++ ex7/runkMeans.m
@@ -3,13 +3,13 @@ function [centroids, idx] = runkMeans(X, initial_centroids, ...
 %RUNKMEANS runs the K-Means algorithm on data matrix X, where each row of X
 %is a single example
 %   [centroids, idx] = RUNKMEANS(X, initial_centroids, max_iters, ...
-%   plot_progress) runs the K-Means algorithm on data matrix X, where each 
+%   plot_progress) runs the K-Means algorithm on data matrix X, where each
 %   row of X is a single example. It uses initial_centroids used as the
-%   initial centroids. max_iters specifies the total number of interactions 
-%   of K-Means to execute. plot_progress is a true/false flag that 
-%   indicates if the function should also plot its progress as the 
-%   learning happens. This is set to false by default. runkMeans returns 
-%   centroids, a Kxn matrix of the computed centroids and idx, a m x 1 
+%   initial centroids. max_iters specifies the total number of interactions
+%   of K-Means to execute. plot_progress is a true/false flag that
+%   indicates if the function should also plot its progress as the
+%   learning happens. This is set to false by default. runkMeans returns
+%   centroids, a Kxn matrix of the computed centroids and idx, a m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -33,16 +33,16 @@ idx = zeros(m, 1);
 
 % Run K-Means
 for i=1:max_iters
-    
+
     % Output progress
     fprintf('K-Means iteration %d/%d...\n', i, max_iters);
     if exist('OCTAVE_VERSION')
         fflush(stdout);
     end
-    
+
     % For each example in X, assign it to the closest centroid
     idx = findClosestCentroids(X, centroids);
-    
+
     % Optionally, plot progress here
     if plot_progress
         plotProgresskMeans(X, centroids, previous_centroids, idx, K, i);
@@ -50,7 +50,7 @@ for i=1:max_iters
         fprintf('Press enter to continue.\n');
         pause;
     end
-    
+
     % Given the memberships, compute new centroids
     centroids = computeCentroids(X, idx, K);
 end
